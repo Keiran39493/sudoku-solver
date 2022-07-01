facceef0afd4148a4366f4dbb81223b1f8571521
Keiran39493 <74584957+Keiran39493@users.noreply.github.com>
2022-07-02 00:27:32 +0800

    Initial commit

---
+++ solver.py
@@ -0,0 +1,89 @@
+from re import T
+
+
+board = [
+    [7,8,0,4,0,0,1,2,0],
+    [6,0,0,0,7,5,0,0,9],
+    [0,0,0,6,0,1,0,7,8],
+    [0,0,7,0,4,0,2,6,0],
+    [0,0,1,0,5,0,9,3,0],
+    [9,0,4,0,6,0,0,0,5],
+    [0,7,0,3,0,0,0,1,2],
+    [1,2,0,0,0,7,4,0,0],
+    [0,4,9,2,0,6,0,0,7]
+]
+
+def solve(bo):
+    
+    # recursive method
+    find = find_empty(bo)
+    if not find:
+        return True
+    else:
+        row, col = find
+    
+    # loop through 1 to 10, if valid add to board, then recursively try to finish solution 
+    for i in range(1,10):
+        if valid(bo, i, (row,col)):
+            bo[row][col] = i
+            
+            if solve(bo):
+                return True 
+            
+            bo[row][col] = 0
+    
+    # if all numbers are invalid - backtrack    
+    return False
+            
+
+def valid(bo, num, pos):
+    
+    # Check through each element in the row and see if it's equal to the recently added number
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1] != i:
+            return False
+        
+    # Check through each element in the column and see if it's equal to the recently added number
+    for i in range(len(bo[0])):
+        if bo[i][pos[1]] == num and pos[0] != i:
+            return False
+        
+    # Check box / determine which box is which
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+    
+    # Loop through every box 
+    for i in range(box_y * 3, box_y*3 + 3):
+        for j in range(box_x * 3, box_x*3 + 3):
+            if bo[i][j] == num and (i,j) != pos:
+                return False    
+    return True
+        
+def print_board(bo):
+    for i in range(len(bo)):
+        if i % 3 == 0 and i !=0:
+            print("---------------------")
+            
+        for j in range(len(bo[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+                
+            if j == 8:
+                        print(bo[i][j])
+            else:
+                print(str(bo[i][j]) + " ", end="")
+                    
+def find_empty(bo):
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j] == 0:
+                return(i, j) # row, col        
+
+    return None
+
+if __name__ == "__main__":
+    print_board(board)
+    print("")
+    solve(board)
+    print_board(board)
+    
